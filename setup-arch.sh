6eb745674e2140b27bc3106ea67d2202fb67f2d0
neo <neosanora@gmail.com>
2025-07-19 20:40:59 +0700

    now the dotfile can work with dotfile installer

---
+++ setup-arch.sh
@@ -0,0 +1,139 @@
+#!/usr/bin/env bash
+
+# ----------------------------------------------------------
+# Packages
+# ----------------------------------------------------------
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+source $SCRIPT_DIR/share/packages/arch.sh
+
+# ----------------------------------------------------------
+# Colors
+# ----------------------------------------------------------
+
+GREEN='\033[0;32m'
+NONE='\033[0m'
+
+# ----------------------------------------------------------
+# Check if command exists
+# ----------------------------------------------------------
+
+_checkCommandExists() {
+    cmd="$1"
+    if ! command -v "$cmd" >/dev/null; then
+        echo 1
+        return
+    fi
+    echo 0
+    return
+}
+
+# ----------------------------------------------------------
+# Check if package is already installed
+# ----------------------------------------------------------
+
+_isInstalled() {
+    package="$1"
+    check="$(sudo pacman -Qs --color always "${package}" | grep "local" | grep "${package} ")"
+    if [ -n "${check}" ]; then
+        echo 0
+        return #true
+    fi
+    echo 1
+    return #false
+}
+
+# ----------------------------------------------------------
+# Install yay
+# ----------------------------------------------------------
+
+_installYay() {
+    _installPackages "base-devel"
+    SCRIPT=$(realpath "$0")
+    temp_path=$(dirname "$SCRIPT")
+    git clone https://aur.archlinux.org/yay.git $download_folder/yay
+    cd $download_folder/yay
+    makepkg -si
+    cd $temp_path
+    echo ":: yay has been installed successfully."
+}
+
+# ----------------------------------------------------------
+# Install packages
+# ----------------------------------------------------------
+
+_installPackages() {
+    toInstall=()
+    for pkg; do
+        if [[ $(_isInstalled "${pkg}") == 0 ]]; then
+            echo ":: ${pkg} is already installed."
+            continue
+        fi
+        toInstall+=("${pkg}")
+    done
+    if [[ "${toInstall[@]}" == "" ]]; then
+        return
+    fi
+    if [[ ! ${toInstall[@]} == "cargo" ]]; then
+        printf "Package not installed:\n%s\n" "${toInstall[@]}"
+    fi
+    yay --noconfirm -S "${toInstall[@]}"
+}
+
+# ----------------------------------------------------------
+# Header
+# ----------------------------------------------------------
+
+clear
+echo -e "${GREEN}"
+cat <<"EOF"
+   ____    __          
+  / __/__ / /___ _____ 
+ _\ \/ -_) __/ // / _ \
+/___/\__/\__/\_,_/ .__/
+                /_/    
+ML4W Hyprland Starter for Arch based distros
+
+EOF
+echo -e "${NONE}"
+
+# ----------------------------------------------------------
+# Content
+# ----------------------------------------------------------
+
+while true; do
+    read -p "DO YOU WANT TO START THE PACKAGE INSTALLATION NOW? (Yy/Nn): " yn
+    case $yn in
+        [Yy]*)
+            echo ":: Installation started."
+            echo
+            break
+            ;;
+        [Nn]*)
+            echo ":: Installation canceled"
+            exit
+            break
+            ;;
+        *)
+            echo ":: Please answer yes or no."
+            ;;
+    esac
+done
+
+# Install yay if needed
+if [[ $(_checkCommandExists "yay") == 0 ]]; then
+    echo ":: yay is already installed"
+else
+    echo ":: The installer requires yay. yay will be installed now"
+    _installYay
+fi
+
+# Packages
+_installPackages "${packages[@]}"
+
+# ----------------------------------------------------------
+# Completed
+# ----------------------------------------------------------
+
+echo ":: Installation complete."
+echo ":: Ready to install the dotfiles with the Dotfiles Installer."
---
+++ share/packages/fedora.sh
@@ -0,0 +1,56 @@
+# ----------------------------------------------------------
+# Packages
+# ----------------------------------------------------------
+
+packages=(
+  # === Tools Umum ===
+  "wget"
+  "unzip"
+  "git"
+  "jq"
+  "vim"
+  "neovim"
+  "gum"
+  "fastfetch"
+
+  # === Font ===
+  "ttf-font-awesome"
+  "ttf-fira-sans"
+  "ttf-fira-code"
+  "ttf-firacode-nerd"
+
+  # === Display Manager & WM ===
+  "hyprland"
+  "xdg-desktop-portal-hyprland"
+  "qt5-wayland"
+  "qt6-wayland"
+
+  # === Aplikasi GUI & System ===
+  "kitty"           # Default terminal emulator
+  "alacritty"       # GPU-accelerated terminal emulator
+  "thunar"          # file manager
+  "firefox"         # browser
+  "pavucontrol"     # audio control
+  "dunst"           # notifikasi
+  "wlogout"         # logout dialog
+  "flatpak"         # package manager alternatif
+
+  # === Audio / Media ===
+  "wireplumber"
+  "brightnessctl"
+  "networkmanager"
+  "pamixer"      # audio mixer
+
+  # === Bar, Menu, Lock ===
+  "eww"          # status bar
+  "wofi"         # app launcher
+  "hyprpaper"    # wallpaper
+  "hyprlock"     # lock screen
+
+  # === Screenshot ===
+  "grim"         # screenshot tool
+  "slurp"        # region selector
+
+  # === Monitoring ===
+  "nvtop"        # GPU usage
+)
