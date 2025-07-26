bcd13775eb0e2cab9c3ecc579b398147b691815b
neosanora <neosanora@gmail.com>
2025-07-26 18:47:19 +0700

    Create aur-package.sh

---
+++ share/packages/aur-package.sh
@@ -0,0 +1,12 @@
+# aur-package.sh
+
+aur_packages=(
+  "brave-bin"               # Brave browser dari binary
+  "xpadneo-dkms-git"        # Driver Bluetooth Xbox controller
+
+  # === Audio / Media ===
+  "pipewire-jack"   # JACK support via PipeWire
+  "pipewire-alsa"   # ALSA support via PipeWire
+  "pipewire-pulse"  # PulseAudio replacement via PipeWire
+  "qjackctl"        # GUI control untuk JACK server
+)
